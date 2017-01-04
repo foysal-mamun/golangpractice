0bd183ee62fe11906f6ce41c7a2d08e46d9fe70b
Foysal Mamun <sal_mdn@yahoo.com>
2017-01-04 11:31:52 +0600

    week 3

--- coursera/algorithmic-toolbox/week-3/maximizing-the-value-of-a-loot/mtvoal.go
+++ coursera/algorithmic-toolbox/week-3/maximizing-the-value-of-a-loot/mtvoal.go
@@ -1,12 +1,42 @@
 package maximizing_the_value_of_a_loot
 
+import "sort"
+
 type loot struct {
 	value  int
 	weight int
 }
 
-func NativeSolution(loots []loot) int {
-	totalValue := 0
+type lootSort []loot
 
-	return totalValue
+func (l lootSort) Len() int           { return len(l) }
+func (l lootSort) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l lootSort) Less(i, j int) bool { return l[i].value/l[i].weight >= l[j].value/l[j].weight }
+
+func NativeSolution(loots []loot, capacity int) int {
+
+	sort.Sort(lootSort(loots))
+
+	return getMaxLootNative(loots, capacity)
+}
+
+func getMaxLootNative(loots []loot, capacity int) int {
+
+	if len(loots) <= 0 || capacity <= 0 {
+		return 0
+	}
+
+	l := loots[0]
+
+	takenWeight := 0
+
+	if capacity < l.weight {
+		takenWeight = capacity * l.value / l.weight
+		capacity = 0
+	} else {
+		takenWeight = l.value
+		capacity = capacity - l.weight
+	}
+
+	return takenWeight + getMaxLootNative(loots[1:], capacity)
 }
--- coursera/algorithmic-toolbox/week-3/maximizing-the-value-of-a-loot/mtvoal_test.go
+++ coursera/algorithmic-toolbox/week-3/maximizing-the-value-of-a-loot/mtvoal_test.go
@@ -5,15 +5,17 @@ import "testing"
 func TestNativeSolution(t *testing.T) {
 	var testCases = []struct {
 		loots    []loot
+		capacity int
 		expected int
 	}{
-		{[]loot{{60, 20}, {100, 50}, {120, 30}}, 180},
+		{[]loot{{60, 20}, {100, 50}, {120, 30}}, 50, 180},
+		{[]loot{{500, 30}}, 10, 166},
 	}
 
 	for _, tc := range testCases {
-		actual := NativeSolution(tc.loots)
+		actual := NativeSolution(tc.loots, tc.capacity)
 		if actual != tc.expected {
-			t.Errorf("NativeSolution(%v): excepted=%d, actual=%d", tc.loots, tc.expected, actual)
+			t.Errorf("NativeSolution(%v, %d): excepted=%d, actual=%d", tc.loots, tc.capacity, tc.expected, actual)
 		}
 	}
 
